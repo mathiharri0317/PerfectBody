3e850cf38ae2880fd49e3d8e31be6fa923262c3b
mathiharri0317 <harri_01_@hotmail.com>
2025-03-13 09:40:06 -0500

    ventana.js

--- ventana.js
+++ ventana.js
@@ -1,12 +1,13 @@
-function mostrarModal(event) {
-    event.preventDefault(); // Evita el envío automático del formulario
-    document.getElementById("miModal").style.display = "flex";
+
+function mostrarAlerta() {
+    let modal = document.getElementById("miModal");
+    modal.style.display = "block";
+    return false; // Evita que el formulario se envíe automáticamente
 }
 
 function cerrarModal() {
-    window.location.href = "index.html"; // Reemplaza con la URL de destino
-}
-
+    let modal = document.getElementById("miModal");
+    m
 document.addEventListener("DOMContentLoaded", function() {
     const formulario = document.querySelector("form");
     if (formulario) {
