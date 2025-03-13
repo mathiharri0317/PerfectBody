6caea5d6838a0b37ca6bef805bab1582144d6568
mathiharri0317 <harri_01_@hotmail.com>
2025-03-13 10:15:00 -0500

    Update ventana.js

--- ventana.js
+++ ventana.js
@@ -1,11 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("miModal");
+    const closeModalBtn = document.querySelector(".close-btn");
     const formulario = document.querySelector("form");
-    if (formulario) {
-        formulario.addEventListener("submit", function(event) {
-            event.preventDefault(); // Evita que la página se recargue
-            
-            var modal = new bootstrap.Modal(document.getElementById("miModal"));
-            modal.show();
-        });
+
+    function mostrarAlerta(event) {
+        event.preventDefault(); // Evita que la página se recargue
+        modal.style.display = "block";
     }
+
+    function cerrarModal() {
+        modal.style.display = "none";
+    }
+
+    formulario.addEventListener("submit", mostrarAlerta);
+    closeModalBtn.addEventListener("click", cerrarModal);
+
+    // Cierra el modal si se hace clic fuera de él
+    window.addEventListener("click", function(event) {
+        if (event.target === modal) {
+            cerrarModal();
+        }
+    });
 });
+
