39d3345b01ad84f20cb8723287660295878f799c
mathiharri0317 <harri_01_@hotmail.com>
2025-03-13 09:59:32 -0500

    Update ventana.js

--- ventana.js
+++ ventana.js
@@ -1,15 +1,17 @@
-function mostrarAlerta() {
+
+function mostrarAlerta(event) {
+    event.preventDefault();
     var modal = new bootstrap.Modal(document.getElementById("miModal"));
     modal.show();
-}
-
-function redirigir() {
-    window.location.href = "login.html";
+    
+    document.getElementById("modalAceptar").addEventListener("click", function() {
+        window.location.href = "login.html";
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     const formulario = document.querySelector("form");
     if (formulario) {
-        formulario.addEventListener("submit", mostrarModal);
+        formulario.addEventListener("submit", mostrarAlerta);
     }
 });
